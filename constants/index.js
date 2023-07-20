be83d113b90c485ef0881c1bb9bf026494e5b73a
rezaazimi66 <34487586+rezaazimi66@users.noreply.github.com>
2023-07-20 13:52:35 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,251 +1,72 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xbbb5959752007b8474244740cf8248dcd5862e9d";
-export const TOKEN_CONTRACT_ABI = [
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "owner",
-				"type": "address"
-			},
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "spender",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "value",
-				"type": "uint256"
-			}
-		],
-		"name": "Approval",
-		"type": "event"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "spender",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
-		"name": "approve",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
-		"name": "burn",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
-		"name": "mint",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "recipient",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
-		"name": "transfer",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "from",
-				"type": "address"
-			},
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "to",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "value",
-				"type": "uint256"
-			}
-		],
-		"name": "Transfer",
-		"type": "event"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "sender",
-				"type": "address"
-			},
-			{
-				"internalType": "address",
-				"name": "recipient",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
-		"name": "transferFrom",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			},
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"name": "allowance",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"name": "balanceOf",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "decimals",
-		"outputs": [
-			{
-				"internalType": "uint8",
-				"name": "",
-				"type": "uint8"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "name",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "symbol",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "totalSupply",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	}
-];
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+ 
+// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.0.0/contracts/token/ERC20/IERC20.sol
+interface IERC20 {
+   function totalSupply() external view returns (uint);
+ 
+   function balanceOf(address account) external view returns (uint);
+ 
+   function transfer(address recipient, uint amount) external returns (bool);
+ 
+   function allowance(address owner, address spender) external view returns (uint);
+ 
+   function approve(address spender, uint amount) external returns (bool);
+ 
+   function transferFrom(
+       address sender,
+       address recipient,
+       uint amount
+   ) external returns (bool);
+ 
+   event Transfer(address indexed from, address indexed to, uint value);
+   event Approval(address indexed owner, address indexed spender, uint value);
+}
+ 
+ 
+ 
+contract ERC20 is IERC20 {
+   uint public totalSupply;
+   mapping(address => uint) public balanceOf;
+   mapping(address => mapping(address => uint)) public allowance;
+   string public name = "requemfor";
+   string public symbol = "drm";
+   uint8 public decimals = 18;
+ 
+   function transfer(address recipient, uint amount) external returns (bool) {
+       balanceOf[msg.sender] -= amount;
+       balanceOf[recipient] += amount;
+       emit Transfer(msg.sender, recipient, amount);
+       return true;
+   }
+ 
+   function approve(address spender, uint amount) external returns (bool) {
+       allowance[msg.sender][spender] = amount;
+       emit Approval(msg.sender, spender, amount);
+       return true;
+   }
+ 
+   function transferFrom(
+       address sender,
+       address recipient,
+       uint amount
+   ) external returns (bool) {
+       allowance[sender][msg.sender] -= amount;
+       balanceOf[sender] -= amount;
+       balanceOf[recipient] += amount;
+       emit Transfer(sender, recipient, amount);
+       return true;
+   }
+ 
+   function mint(uint amount) external {
+       balanceOf[msg.sender] += amount;
+       totalSupply += amount;
+       emit Transfer(address(0), msg.sender, amount);
+   }
+ 
+   function burn(uint amount) external {
+       balanceOf[msg.sender] -= amount;
+       totalSupply -= amount;
+       emit Transfer(msg.sender, address(0), amount);
+   }
+}
